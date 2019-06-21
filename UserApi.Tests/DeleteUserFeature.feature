3f0a06c410ae356d5149a3d0587e16b36cee3c21
Agnaldo <agnaldo.filho@basis.com.br>
2019-06-20 22:39:10 -0300

    initial commit

---
+++ UserApi.Tests/DeleteUserFeature.feature
@@ -0,0 +1,15 @@
+﻿Feature: DeleteUserFeature
+	In order delete a user by Id
+	As a API consumer
+	I want to be to delete a user's information through the API
+
+Scenario: Deleting user by Id
+	Given that a user exists in the system
+	When I request to delete the user by Id
+	Then the user should be deleted
+	And the response status code is '200 OK'
+
+Scenario: Deleting non-existing user by Id
+	Given that a user does not exist in the system
+	When I request to delete the user by Id
+	Then the response status code is '404 Not Found'
---
+++ UserApi.Tests/DeleteUserFeatureSteps.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using TechTalk.SpecFlow;
+using UserApi.Api.Controllers;
+using UserApi.Data.Interfaces;
+
+namespace UserApi.Tests
+{
+    [Binding]
+    public class DeleteUserFeatureSteps
+    {
+        ScenarioContext context = ScenarioContext.Current;
+
+        [When(@"I request to delete the user by Id")]
+        public void WhenIRequestToDeleteTheUserById()
+        {
+            UsersController usersController = context.Get<UsersController>();
+            var response = usersController.DeleteUser(1);
+            context.Set(response);
+        }
+
+        [Then(@"the user should be deleted")]
+        public void ThenTheUserShouldBeDeleted()
+        {
+            var repository = context.Get<IUsersRepository>();
+            Assert.IsTrue(repository.GetById(1) == null);
+        }
+
+    }
+}
